bb0511b4b4b5abbcda9e30e0c181ba09b64a1e22
DirectumRXUser <directumrxuser@c.ru>
2024-06-14 11:12:58 +0300

    Добавил св-во Индекс

--- DirRX.HelpDesk/DirRX.HelpDesk.ClientBase/TypeRequest/TypeRequestActions.cs
+++ DirRX.HelpDesk/DirRX.HelpDesk.ClientBase/TypeRequest/TypeRequestActions.cs
@@ -7,4 +7,18 @@ using DirRX.HelpDesk.TypeRequest;
 
 namespace DirRX.HelpDesk.Client
 {
+  partial class TypeRequestActions
+  {
+    public virtual void LinkDocument(Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+      
+    }
+
+    public virtual bool CanLinkDocument(Sungero.Domain.Client.CanExecuteActionArgs e)
+    {
+      return true;
+    }
+
+  }
+
 }
